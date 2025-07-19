3edfe77555d2a44a45533098245da2cb936f7603
strivehardest <ibrahimah2011@hotmail.com>
2025-07-19 21:08:31 +0000

    Update urls.py

--- Introduction_to_Django/LibraryProject/relationship_app/urls.py
+++ Introduction_to_Django/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,20 @@
-from django.urls import path
-from .views import login_view, logout_view, register_view
+# relationship_app/urls.py
 
-urlpatterns += [
+from django.urls import path
+from .views import (
+    login_view,
+    logout_view,
+    register_view,
+    admin_view,
+    librarian_view,
+    member_view,
+)
+
+urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
 ]
