115e38ce7c3986f45bc624961c32e37022667a96
strivehardest <ibrahimah2011@hotmail.com>
2025-07-19 20:47:16 +0000

    Create views.py

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,3 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
