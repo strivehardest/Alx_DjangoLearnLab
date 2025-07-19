2fa75ce449b6d85b4f28d3c80dec8e1497ae029c
strivehardest <ibrahimah2011@hotmail.com>
2025-07-19 19:47:25 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,3 +5,12 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  # FBV
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # CBV
 ]
+
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
+]
