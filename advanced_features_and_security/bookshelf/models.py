eb674abd353ff55bddcd6a82b7396fc1aaff1e44
strivehardest <ibrahimah2011@hotmail.com>
2025-07-24 15:46:41 +0000

    Add files via upload

---
+++ advanced_features_and_security/bookshelf/models.py
@@ -0,0 +1,31 @@
+
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import BaseUserManager
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True or extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_staff=True and is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
