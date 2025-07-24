5118d5e831ae53bf67c9aae44bf26a8f4a5ed4a0
strivehardest <ibrahimah2011@hotmail.com>
2025-07-24 15:38:16 +0000

    Add files via upload

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,4 @@
+class CustomUser(AbstractUser):
+    email = models.EmailField(('email address'), unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
