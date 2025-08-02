527f050710fdbc74b4e5ee6162721b2fb9206752
strivehardest <ibrahimah2011@hotmail.com>
2025-08-02 11:04:05 +0000

    Add files via upload

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -15,3 +15,11 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+
+urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('', include('api.urls')),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -17,3 +17,15 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
