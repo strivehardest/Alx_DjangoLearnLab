fc0f9e32aa9d92dc8d58029652074dbe7db89585
strivehardest <ibrahimah2011@hotmail.com>
2025-08-11 17:40:02 +0000

    Add files via upload

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -89,4 +89,17 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
 
+from django.urls import path
+from .views import (
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+)
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+]
+
 
