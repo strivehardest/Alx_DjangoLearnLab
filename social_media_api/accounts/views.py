94802dc3e50f7e0e26178ff2f36566df23daa07d
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-08-22 15:30:01 +0000

    Add files via upload

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,43 +1,28 @@
-from django.contrib.auth import login
-from rest_framework.views import APIView
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status, permissions
-
+from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
-class RegisterView(APIView):
-    permission_classes = [permissions.AllowAny]
 
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class LoginView(APIView):
-    permission_classes = [permissions.AllowAny]
 
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-        login(request, user)
-        return Response(
-            {
-                "username": user.username,
-                "token": serializer.validated_data["token"]
-            },
-            status=status.HTTP_200_OK
-        )
-
-class ProfileView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request):
-        return Response(UserSerializer(request.user).data)
-
-    def patch(self, request):
-        serializer = UserSerializer(request.user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        return Response({
+            "token": serializer.validated_data["token"],
+            "username": serializer.validated_data["user"].username
+        }, status=status.HTTP_200_OK)
